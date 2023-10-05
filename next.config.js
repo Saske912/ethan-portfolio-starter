1c81cc6c6ece542172ee383d3e936605136f8e46
mihail.prigorodov <mikhail.prigorodov@facecast.net>
2023-10-05 21:55:25 +0300

    about in progress

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { isServer }) => {
+    const path = require('path');
+
+    // Добавьте алиас для корневой директории
+    config.resolve.alias['@'] = path.join(__dirname);
+
+    // Можете также добавить другие алиасы по вашему усмотрению
+    // config.resolve.alias['components'] = path.join(__dirname, 'components');
+
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- pages/about/index.js
+++ pages/about/index.js
@@ -15,6 +15,9 @@ import {
   SiAdobephotoshop,
 } from "react-icons/si";
 
+import Avatar from '@/components/Avatar'
+import Circles from '../../components/Circles'
+import { useState } from 'react';
 
 //  data
 const aboutData = [
@@ -88,8 +91,84 @@ const aboutData = [
   },
 ];
 
+import { motion } from "framer-motion";
+import { fadeIn } from '../../variants'
+
+import CountUp from "react-countup";
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+    <Circles />
+    {/* avatar img */}
+    <motion.div variants={fadeIn('right', 0.2)}
+      initial="hidden"
+      animate="show"
+      exit="hidden"
+      className="hidden xl:flex  absolute bottom-0 -left-[370px]">
+      <Avatar />
+    </motion.div>
+    <div className="container mx-auto h-full flex flex-col 
+    items-center xl:flex-row gap-x-6">
+      <div className="flex-1 flex flex-col justify-center">
+        {/* text */}
+        <div>
+          <h2 className="h2">Captivating <span className="text-accent">stories</span> birth magnificent design.</h2>
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">10 year ago, I began freelancing as a developer. Since then I've done
+            remote work for agencies, consulted for startups, and collaborate on digital
+            products for business and consumer use.
+          </p>
+          {/* counters */}
+          <div>
+            <div>
+              {/* experience */}
+              <div>
+                experience
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* info */}
+      </div>
+      <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+        <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+          {aboutData.map((item, i) => {
+            return <div key={i} className={
+              `${index === i &&
+              'text-accent after:w-[100%] after:bg-accent after:transition-all duration-300'} cursor-pointer capitalize
+            xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute
+            after:-bottom-1 after:left-0`}
+              onClick={() => setIndex(i)}
+            >
+              {item.title}
+            </div>;
+          })}
+        </div>
+        <div className="py-2 xl:py-6 flex flex-col gap-x-2
+        gap-y-4 items-center xl:items-start">
+          {aboutData[index].info.map((item, i) => {
+            return <div key={i} className="flex-1 flex flex-col md:flex-row
+            max-w-max gap-x-2 items-center text-white/60">
+              {/* title */}
+              <div className="font-light mb-2 md:mb-0">
+                {item.title}
+              </div>
+              <div className="hidden md:flex">-</div>
+              <div>{item.stage}</div>
+              <div className="flex gap-x-4">
+                {/* icons */}
+                {item.icons?.map((icon, i) => {
+                  return <div key={i} className="text-2xl text-white">
+                    {icon}
+                  </div>;
+                })}
+              </div>
+            </div>
+          })}
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default About;
